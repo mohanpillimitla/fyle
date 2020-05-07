d41c92dd4995eff692201f1336d3c890adc9515a
harsha <harshavardhanpillimitla143@gmail.com>
2020-05-07 17:52:44 +0530

    'initial'

---
+++ mysite/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+urlpatterns = [
+               path('',home,name='home')
+
+
+
+    ]
+
+urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)

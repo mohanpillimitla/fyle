120440ee7d0c0851559ae52f4d2a1bb568e9ea2b
harsha <harshavardhanpillimitla143@gmail.com>
2020-05-07 20:08:04 +0530

    'hello'

--- fyle/settings.py
+++ fyle/settings.py
@@ -120,8 +120,3 @@ USE_TZ = True
 
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-MEDIA_URL='/images/'
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
